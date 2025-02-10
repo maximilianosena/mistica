dafe6a63d721f43eb11ce170abc2fc9f851095b5
Maximiliano Sena <130717933+maximilianosena@users.noreply.github.com>
2025-02-10 03:07:03 -0300

    jabon individual

--- jabon.js
+++ jabon.js
@@ -2,9 +2,26 @@ let id = localStorage.getItem("id")?localStorage.getItem("id"):1
 
 let url = `https://mistica-back.vercel.app/jabon/${id}`
 
-fetch(url)
+let containerProducto = document.getElementById("containerProducto")
+  function mostrarProducto(info){
+
+    containerProducto.innerHTML+=` <img src="${info.img}" alt="Imagen del producto">
+    <br>
+    <h1>${info.nombre}</h1>
+    <p>Ingredientes: ${info.ingredientes.map(ingrediente => ingrediente).join(', ')}</p>
+    <br>
+    <p>TEXTO DESCRIPCIÓN</p>
+    <button id=${info.id}>Me interesa</button>
+    `
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+  fetch(url)
   .then(response => response.json())
   .then(data =>{
     console.log(data)
-    
+    mostrarProducto(data)
   } )
+ 
+})
